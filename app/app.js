5d94a3a1fd260735ed7e718e261d42ddb118eaeb
Stephen McElhinney <smcelhinney@hubspot.com>
2018-05-01 17:26:06 +0100

    Added a Redis connection example to NodeJS

--- app/app.js
+++ app/app.js
@@ -1,19 +1,33 @@
 // Setup configuration
 require('dotenv').config();
+const {
+  NODE_ENV,
+  PORT,
+  REDIS_PORT_6379_TCP_ADDR: host,
+  REDIS_PORT_6379_TCP_PORT: port
+} = process.env;
 
 const express = require('express');
 const cors = require('cors');
 const debug = require('debug')('appstructio')
+const redis = require('redis')
+const {promisify} = require('util');
 
-const { NODE_ENV, PORT } = process.env;
+const client = redis.createClient({ host, port });
+const getAsync = promisify(client.get).bind(client);
+const setAsync = promisify(client.set).bind(client);
+
+// console.log(process.env);
 
 const app = express();
 // Start server
-app.get('/', (req, res) => {
-  res.send(`Healthcheck at ${new Date()}`);
+app.get('/', async (req, res) => {
+  const redisVal = await getAsync('TEST_VAL');
+  res.send(`Healthcheck at ${new Date()} REDIS VAL: ${redisVal}`);
 });
 
 app.set('port', PORT || 3030);
-app.listen(app.get('port'), () => {
+app.listen(app.get('port'), async () => {
+  await setAsync('TEST_VAL', 'doobydoobydo');
   debug(`Express running on port ${app.get('port')}`);
 });
